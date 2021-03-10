49792871f8dab9fb992430c34d3a80a8bbd11af2
ValentinCecrops <gfj@gmail.com>
2021-03-10 09:52:08 +0100

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,18 @@
+#include <string>
+
+class Player
+{
+private:
+    std::string _name;
+    int _songScore[5];
+    int _score;
+public:
+    void showAverageScore(int scores);
+    void showTotalScore(int scores);
+    void showHighScore(int scores);
+    void showLowScore(int scores);
+    void showScores(int scores);
+    int addScore();
+    Player(std::string name);
+    Player();
+};
